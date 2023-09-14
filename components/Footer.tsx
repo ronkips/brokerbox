e13b8a861ae84147c7eed3ab6f04d50a4ad0f491
ronkips <kipshillash@gmail.com>
2023-09-14 06:43:34 +0300

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,40 @@
-import { CompanyFrame, CompanyText, Copyright, FooterContainer, Logo, Socials, SocialsText, Terms, TermsText } from '@/styles/Footer/Footer';
-import React from 'react';
+import {
+  CompanyFrame,
+  CompanyText,
+  Copyright,
+  FooterContainer,
+  Logo,
+  Socials,
+  SocialsText,
+  Terms,
+  TermsText
+} from "@/styles/Footer/Footer";
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
     <FooterContainer>
-        <Logo></Logo>
-        <CompanyFrame>
-            <CompanyText>Company</CompanyText>
-        <a href= "/">About Us</a>
-        <a href= "/">Why Us</a>
-        </CompanyFrame>
-        <Socials>
-            <SocialsText>Socials</SocialsText>
-        <a href= "/">LinkedIn</a>
-        <a href= "/">Twitter</a>
-        <a href= "/">Instagram</a>
-        </Socials>
-        <Terms>
-            <TermsText>Terms & Conditions</TermsText>
-        <a href= "/">Privacy notice</a>
-        <a href= "/">Cookie policy</a>
-        </Terms>
-        <Copyright> Copyright@Broker Box{new Date().getFullYear()}</Copyright>
+      <Logo></Logo>
+      <CompanyFrame>
+        <CompanyText>Company</CompanyText>
+        <Link href="/">About Us</Link>
+        <Link href="/">Why Us</Link>
+      </CompanyFrame>
+      <Socials>
+        <SocialsText>Socials</SocialsText>
+        <Link href="/">LinkedIn</Link>
+        <Link href="/">Twitter</Link>
+        <Link href="/">Instagram</Link>
+      </Socials>
+      <Terms>
+        <TermsText>Terms & Conditions</TermsText>
+        <Link href="/">Privacy notice</Link>
+        <Link href="/">Cookie policy</Link>
+      </Terms>
+      <Copyright> Copyright@Broker Box{new Date().getFullYear()}</Copyright>
     </FooterContainer>
-  )
-}
+  );
+};
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,18 @@
-import { NavLinks, NavLogo, NavbarContainer } from '@/styles/Navbar/Navbar';
-import React from 'react';
+import { NavLinks, NavLogo, NavbarContainer } from "@/styles/Navbar/Navbar";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
     <NavbarContainer>
-        <NavLogo>
-            <NavLinks>
-                <a href= "/">About Us</a>
-                <a href= "/">Why Us</a>
-            </NavLinks>
-        </NavLogo>
+      <NavLogo>
+        <NavLinks>
+          <Link href="/">About Us</Link>
+          <Link href="/">Why Us</Link>
+        </NavLinks>
+      </NavLogo>
     </NavbarContainer>
-  )
-}
+  );
+};
 
 export default Navbar;
